e7c142ab7b8b974c5e1c8a9da0fc66dc426288a7
Sakarr <ctha.sakar@gmail.com>
2024-05-14 14:22:03 +0545

    Refactor - Codes

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
+let secretNumber = Math.trunc(Math.random() * 20) + 1,
+  score = 20, // State variable.
+  highscore = 0; // State Variable.
 
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value;
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
-*/
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20; // State variable.
-let highscore = 0;
-
+// Function wherever the message needs to updated.
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); // Usually data retieved from user interface are generally string.
-  console.log(guess, typeof guess);
 
+  // When there is no input.
   if (!guess) {
-    // When there is no input.
     displayMessage('⛔ No Number!');
   } else if (guess === secretNumber) {
     // When player wins.
     displayMessage('🎉 Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
 
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
+    // Update highscore.
     if (score > highscore) {
       highscore = score;
+
       document.querySelector('.highscore').textContent = highscore;
-    } // When guess is wrong.
+    }
   } else if (guess !== secretNumber) {
+    // When guess is wrong.
     if (score > 1) {
       displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+
       score--;
+
       document.querySelector('.score').textContent = score;
     } else {
       displayMessage('💥 You lost the game!');
+
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
+// Reset the game when clicked on Again! button.
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
   document.querySelector('.guess').value = '';
+
   displayMessage('Start guessing...');
+
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
